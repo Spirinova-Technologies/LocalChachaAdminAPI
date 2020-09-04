990de730e9793ce1af25a9eb3ee488437865ff89
Rohidas Kadam <rohidask90@gmail.com>
2020-09-04 20:16:09 +0530

    Added Swagger

--- LocalChachaAdminApi/Startup.cs
+++ LocalChachaAdminApi/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 
 namespace LocalChachaAdminApi
@@ -31,6 +32,27 @@ namespace LocalChachaAdminApi
             {
                 c.BaseAddress = new Uri(Configuration.GetValue<string>("localChachaUrl"));
             });
+
+            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "LocalChachaAdmin API",
+                    //Contact = new OpenApiContact
+                    //{
+                    //    Name = "Shayne Boyer",
+                    //    Email = string.Empty,
+                    //    Url = new Uri("https://twitter.com/spboyer"),
+                    //},
+                    //License = new OpenApiLicense
+                    //{
+                    //    Name = "Use under LICX",
+                    //    Url = new Uri("https://example.com/license"),
+                    //}
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +62,12 @@ namespace LocalChachaAdminApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "LocalChacha Admin");
+            });
 
             app.UseHttpsRedirection();
 
